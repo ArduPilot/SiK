75cf4613622017d73300d5b32cc5fc16bb62b9d1
Mike Smith <DrZiplok@me.com>
2011-10-07 00:27:24 -0700

    A simple packet buffer system with support for packet queues.

---
+++ Firmware/radio/pkt_buf.c
@@ -0,0 +1,207 @@
+//
+// Copyright (c) 2011 Michael Smith, All Rights Reserved
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  o Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  o Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include "pkt_buf.h"
+
+/* packet buffer pool */
+__xdata struct pbuf		pbuf_pool[PBUF_POOL_SIZE];
+
+/* packet data pool */
+static __xdata uint8_t		pbuf_data[PBUF_POOL_SIZE][PBUF_MAX_SIZE];
+
+/* packet buffer queues */
+struct pbuf_queue
+{
+	PBufIndex	head;
+	PBufIndex	tail;
+};
+
+static __xdata struct pbuf_queue	pbuf_queues[PBUF_MAX_QUEUES];
+
+
+void
+pbuf_init(void)
+{
+	PBufQueueIndex	queue_index;
+	PBufIndex		buffer_index;
+
+	/* initialise all the packet buffer queues to empty */
+	for (queue_index = 0; queue_index < PBUF_MAX_QUEUES; queue_index++) {
+		pbuf_queues[queue_index].head = PBUF_NULL;
+		pbuf_queues[queue_index].tail = PBUF_NULL;
+	}
+
+	/* free all the packet buffers */
+	for (buffer_index = 0; buffer_index < PBUF_POOL_SIZE; buffer_index++)
+		pbuf_putbuf(buffer_index);
+}
+
+void
+pbuf_queue_add_head(PBufQueueIndex queue_index, PBufIndex buffer_index)
+{
+	__xdata struct pbuf_queue *pq = &pbuf_queues[queue_index];
+	bool		istate;
+	PBufIndex	hi;
+
+	interrupt_disable(istate);
+
+	/* get the next buffer in the queue */
+	hi = pq->head;
+
+	/* make this buffer the head of the queue */
+	pq->head = buffer_index;
+
+	/* if the queue was empty, point the tail to this buffer as well */
+	if (hi == PBUF_NULL)
+		pq->tail = buffer_index;
+
+	/* link this buffer to the next */
+	pbuf_next(buffer_index) = hi;
+
+	interrupt_restore(istate);
+}
+
+void
+pbuf_queue_add_tail(PBufQueueIndex queue_index, PBufIndex buffer_index)
+{
+	__xdata struct pbuf_queue *pq = &pbuf_queues[queue_index];
+	bool		istate;
+	PBufIndex	ti;
+
+	interrupt_disable(istate);
+
+	/* get the last buffer in the queue */
+	ti = pq->head;
+
+	/* make this buffer the last one */
+	pq->tail = buffer_index;
+
+	/* if the queue is not empty */
+	if (ti != PBUF_NULL) {
+		/* link the last buffer to this one */
+		pbuf_next(ti) = buffer_index;
+	} else {
+		/* queue was empty, new buffer is also the head */
+		pq->head = buffer_index;
+	}
+
+	interrupt_restore(istate);
+}
+
+static void
+pbuf_queue_remove(PBufQueueIndex queue_index, PBufIndex previous_index, PBufIndex buffer_index)
+{
+	__xdata struct pbuf_queue *pq = &pbuf_queues[queue_index];
+	__xdata struct pbuf *pb = &pbuf_pool[buffer_index];
+	__xdata struct pbuf *pp = &pbuf_pool[previous_index];
+
+	if (previous_index == PBUF_NULL) {
+		pq->head = pb->next;
+	} else {
+		pp->next = pb->next;
+	}
+	if (pq->tail == buffer_index)
+		pq->tail = previous_index;
+	pb->next = PBUF_NULL;
+}
+
+PBufIndex
+pbuf_queue_remove_head(PBufQueueIndex queue_index)
+{
+	bool		istate;
+	PBufIndex	hi;
+
+	interrupt_disable(istate);
+
+	/* get the first buffer */
+	hi = pbuf_queues[queue_index].head;
+	if (hi != PBUF_NULL)
+		pbuf_queue_remove(queue_index, PBUF_NULL, hi);
+
+	interrupt_restore(istate);
+
+	return hi;
+}
+
+PBufIndex
+pbuf_queue_remove_seq(PBufQueueIndex queue_index, uint8_t seq)
+{
+	bool		istate;
+	PBufIndex	bi, pi;
+
+	interrupt_disable(istate);
+
+	/* walk the queue looking for a matching sequence number */
+	pi = PBUF_NULL;
+	bi = pbuf_queues[queue_index].head;
+	while (bi != PBUF_NULL) {
+		/* on sequence match */
+		if (pbuf_sequence_number(bi) == seq) {
+			/* remove it from the list */
+			pbuf_queue_remove(queue_index, pi, bi);
+			break;
+		}
+		pi = bi;
+		bi = pbuf_next(bi);
+	}
+
+	interrupt_restore(istate);
+
+	return bi;
+}
+
+bool
+pbuf_queue_empty(PBufQueueIndex queue_index)
+{
+	if (pbuf_queues[queue_index].head == PBUF_NULL)
+		return true;
+	return false;
+}
+
+void
+pbuf_copy_to_pbuf(PBufIndex idx, uint8_t ofs, __xdata uint8_t *ptr, PBufDataCount cnt)
+{
+	__xdata uint8_t		*b = pbuf_data_ptr(idx, ofs);
+
+	while (cnt--)
+		*b++ = *ptr++;
+}
+
+void
+pbuf_copy_from_pbuf(PBufIndex idx, uint8_t ofs, __xdata uint8_t *ptr, PBufDataCount cnt)
+{
+	__xdata uint8_t		*b = pbuf_data_ptr(idx, ofs);
+
+	while (cnt--)
+		*ptr++ = *b++;
+}
+
+__xdata uint8_t *
+pbuf_data_ptr(PBufIndex idx, PBufDataCount ofs)
+{
+	return &pbuf_data[idx][ofs];
+}
---
+++ Firmware/radio/pkt_buf.h
@@ -0,0 +1,98 @@
+/* -*- Mode: C; c-basic-offset: 8; -*- */
+//
+// Copyright (c) 2011 Michael Smith, All Rights Reserved
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  o Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  o Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#ifndef PTK_BUF_H_
+#define PTK_BUF_H_
+
+/*
+ * Packet buffers.
+ */
+
+#include "radio.h"
+
+#define	PBUF_NULL	0xff		// "no packet buffer" pointer
+#define PBUF_MAX_SIZE	64		// maximum buffer payload size
+#define PBUF_POOL_SIZE	16		// size of the packet buffer pool
+#define PBUF_MAX_QUEUES	4		// number of pbuf queues
+#define PBUF_QUEUE_FREE	0		// free packet buffers
+#define PBUF_QUEUE_TX	1		// packet buffers waiting to transmit
+#define PBUF_QUEUE_RX	2		// packet buffers received
+
+typedef	uint8_t		PBufIndex;	// packet buffer 'pointer', index into global buffer array(s)
+typedef uint8_t		PBufQueueIndex;	// packet buffer queue 'pointer'
+typedef uint8_t		PBufDataCount;	// count of bytes in a packet buffer
+
+/* initialise the pbuf system */
+extern void		pbuf_init(void);
+
+/*
+ * pbuf queue manipulation
+ *
+ * Note that as the queues are singly linked, it is not possible to remove a buffer from the
+ * tail of a queue.
+ */
+extern void		pbuf_queue_add_head(PBufQueueIndex qi, PBufIndex pi);
+extern void		pbuf_queue_add_tail(PBufQueueIndex qi, PBufIndex pi);
+extern PBufIndex	pbuf_queue_remove_head(PBufQueueIndex qi);
+extern PBufIndex	pbuf_queue_remove_seq(PBufQueueIndex qi, uint8_t seq);
+extern bool		pbuf_queue_empty(PBufQueueIndex qi);
+
+/* copy data from an arbitrary buffer to a pbuf */
+extern void		pbuf_copy_to_pbuf(PBufIndex idx, uint8_t ofs, __xdata uint8_t *ptr, PBufDataCount cnt);
+
+/* copy data from a pbuf to an arbitrary buffer */
+extern void 		pbuf_copy_from_pbuf(PBufIndex idx, uint8_t ofs, __xdata uint8_t *ptr, PBufDataCount cnt);
+
+/* returns a pointer in the data buffer for a given packet buffer */
+extern __xdata uint8_t	*pbuf_data_ptr(PBufIndex idx, PBufDataCount ofs);
+
+/*
+ * Packet buffer accessor macros.
+ *
+ * Use these macros and packet buffer indices rather than pointers to buffers
+ * to access fields in the packet buffer header.  This will make it easier to
+ * e.g. separate the fields at a later stage.
+ */
+#define pbuf_getbuf()		pbuf_queue_remove_head(PBUF_QUEUE_FREE);
+#define pbuf_putbuf(_idx)	pbuf_queue_add_head(PBUF_QUEUE_FREE, _idx);
+
+#define	pbuf_size(_idx)				pbuf_pool[_idx].data_size
+#define	pbuf_next(_idx)				pbuf_pool[_idx].next
+#define	pbuf_sequence_number(_idx)		pbuf_pool[_idx].sequence_number
+
+struct pbuf
+{
+	PBufIndex	next;			// next packet buffer in queue
+	uint8_t		sequence_number;	// packet sequence number in the tx/rx stream
+	PBufDataCount	data_size;		// number of bytes in the packet
+};
+extern __xdata struct pbuf		pbuf_pool[];
+
+
+
+#endif /* PTK_BUF_H_ */
