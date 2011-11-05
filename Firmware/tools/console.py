4da945759897d5782a7ca6a449785288ff69ec64
Andrew Tridgell <tridge@samba.org>
2011-11-05 14:35:04 +1100

    a simple serial console

---
+++ Firmware/tools/console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# a trivial serial console
+
+import serial, sys, optparse, fdpexpect
+
+parser = optparse.OptionParser("console")
+parser.add_option("--baudrate", type='int', default=115200, help='baud rate')
+
+opts, args = parser.parse_args()
+
+if len(args) != 1:
+    print("usage: console.py <DEVICE>")
+    sys.exit(1)
+
+device = args[0]
+
+port = serial.Serial(device, opts.baudrate, timeout=0,
+                     dsrdtr=False, rtscts=False, xonxoff=False)
+
+ser = fdpexpect.fdspawn(port.fileno(), maxread=1)
+print("Connecting (use ^] to exit)")
+ser.interact()
+
