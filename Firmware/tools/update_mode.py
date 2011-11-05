a80617c00e06f652d0baa40d57c777768f938c44
Andrew Tridgell <tridge@samba.org>
2011-11-05 14:35:44 +1100

    put a HM-TRP into update mode using AT&UPDATE

---
+++ Firmware/tools/update_mode.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# put a HopeRF into update mode
+
+import serial, sys, optparse, time, fdpexpect
+
+parser = optparse.OptionParser("update_mode")
+parser.add_option("--baudrate", type='int', default=115200, help='baud rate')
+
+opts, args = parser.parse_args()
+
+if len(args) == 0:
+    print("usage: update_mode.py <DEVICE...>")
+    sys.exit(1)
+
+
+def update_mode(device):
+    '''put a HM-TRP into update mode'''
+    port = serial.Serial(device, opts.baudrate, timeout=0,
+                         dsrdtr=False, rtscts=False, xonxoff=False)
+    ser = fdpexpect.fdspawn(port.fileno(), logfile=sys.stdout)
+    ser.send('+++')
+    time.sleep(1)
+    ser.send('\r\nATI\r\n')
+    try:
+        ser.expect(['OK','SiK .* on HM-TRP'], timeout=2)
+    except fdpexpect.TIMEOUT:
+        print("timeout")
+        return
+    ser.send('AT&UPDATE\r\n')
+    try:
+        buf = ser.read_nonblocking(100, timeout=1)
+    except pexpect.TIMEOUT:
+        pass
+    port.close()
+
+
+for d in args:
+    print("Putting %s into update mode" % d)
+    update_mode(d)
+    
