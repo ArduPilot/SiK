bed70f109ecb56b84c087e150ac98449f9bb9717
Mike Smith <DrZiplok@me.com>
2011-11-06 11:43:41 -0800

    Add more documentation on the bootloader protocol, for the sake of any poor sod that comes along later.

--- Firmware/bootloader/bootloader.h
+++ Firmware/bootloader/bootloader.h
@@ -38,28 +38,61 @@
 #include <stdint.h>
 #include "cdt.h"
 
-// Basic flash protocol, originally inspired by the STK500 protocol
+// SiK bootloader flash update protocol.
 //
-// Packet is command byte, command data, EOC.  Reply is [data,] OK or FAILED.
+// Command format:
+//
+// 	<opcode>[<command_data>]<EOC>
+//
+// Reply format:
+//
+//	[<reply_data>]<INSYNC><status>
+//
+// The <opcode> and <status> values come from the PROTO_ defines below,
+// the <*_data> fields is described only for opcodes that transfer data;
+// in all other cases the field is omitted.
+//
+// Expected workflow is:
+//
+// GET_SYNC			verify that the board is present
+// GET_DEVICE			determine which board (select firmware to upload)
+// CHIP_ERASE			clear the program area
+// loop:
+//	LOAD_ADDRESS		set address for programming fragment
+//	loop:
+//		PROG_MULTI	program portion of fragment
+// loop:
+//	LOAD_ADDRESS		set address for verifying program fragment
+//	loop:
+//		READ_MULTI	verify portion of fragment
+// PARAM_ERASE			optional - clear flash scratch/parameter page
+// RESET			resets chip and starts application
 //
 #define PROTO_OK		0x10	// 'ok' response
 #define PROTO_FAILED		0x11	// 'fail' response
-#define PROTO_INSYNC		0x12	// 'in sync' response to GET_SYNC
+#define PROTO_INSYNC		0x12	// 'in sync' byte sent before status
 
 #define PROTO_EOC		0x20	// end of command
 #define PROTO_GET_SYNC		0x21	// NOP for re-establishing sync
 #define PROTO_GET_DEVICE	0x22	// get device ID bytes
+					//    <reply_data>: <board ID><frequency code>
 #define PROTO_CHIP_ERASE	0x23	// erase program area
 #define PROTO_LOAD_ADDRESS	0x24	// set next programming address
-#define PROTO_PROG_FLASH	0x25	// write byte at address + increment
-#define PROTO_READ_FLASH	0x26	// read byte at address + increment
+					//    <command_data>: <lowbyte><highbyte>
+#define PROTO_PROG_FLASH	0x25	// write byte at address + increment address
+					//    <command_data>: <databyte>
+#define PROTO_READ_FLASH	0x26	// read byte at address + increment address
+					//    <reply_data>: <databyte>
 #define PROTO_PROG_MULTI	0x27	// write up to PROTO_PROG_MULTI_MAX bytes at address + increment
+					//    <command_data>: <count><databytes>
 #define PROTO_READ_MULTI	0x28	// read up to 255 bytes at address + increment
+					//    <command_data>: <count>
+					//    <reply_data>: <databytes>
 #define PROTO_PARAM_ERASE	0x29	// erase the parameter flash
 
-#define PROTO_REBOOT		0x30	// reboot the board & start the app
+#define PROTO_REBOOT		0x30	// complet the program cycle, reboot the board & start the app
 
-#define PROTO_PROG_MULTI_MAX	64	// maximum PROG_MULTI size - must fit in DSEG
+#define PROTO_PROG_MULTI_MAX	64	// maximum PROG_MULTI size
 #define PROTO_READ_MULTI_MAX	255	// size of the size field
 
 #endif // _BOOTLOADER_H_
