97296d489ff86b7c653f9de97f2d4c7d1e57852a
Mike Smith <DrZiplok@me.com>
2011-11-05 23:57:04 -0700

    Bump the parameter format version, since the serial speed encodings changed.

--- Firmware/radio/parameters.h
+++ Firmware/radio/parameters.h
@@ -56,7 +56,7 @@ enum ParamID {
         PARAM_MAX			// must be last
 };
 
-#define PARAM_FORMAT_CURRENT	0x14UL				///< current parameter format ID
+#define PARAM_FORMAT_CURRENT	0x15UL				///< current parameter format ID
 
 /// Parameter type.
 ///
