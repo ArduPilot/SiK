4ed8577c771d284db679b35da519509b52141e54
Andrew Tridgell <tridge@samba.org>
2012-01-08 14:38:04 +1100

    added crlf and counter options to pattern.py

--- Firmware/tools/pattern.py
+++ Firmware/tools/pattern.py
@@ -8,6 +8,8 @@ parser.add_option("--baudrate", type='int', default=115200, help='baud rate')
 parser.add_option("--delay", type='float', default=0.0, help='delay between lines')
 parser.add_option("--pattern", type='str', default='0123456789', help='pattern to send')
 parser.add_option("--echo", action='store_true', default=False, help='echo any bytes received')
+parser.add_option("--crlf", action='store_true', default=False, help='add crlf')
+parser.add_option("--counter", action='store_true', default=False, help='add counter')
 
 
 opts, args = parser.parse_args()
@@ -21,15 +23,23 @@ device = args[0]
 port = serial.Serial(device, opts.baudrate, timeout=0,
                      dsrdtr=False, rtscts=False, xonxoff=False)
 
+counter = 0
 while True:
     try:
-        port.write(opts.pattern + '\r\n')
+        buf = opts.pattern[:]
+        if opts.counter:
+            buf += "%02u" % (counter % 100)
+        if opts.crlf:
+            buf += '\r\n'
+        port.write(buf)
         if opts.echo:
             try:
                 buf = port.read()
                 sys.stdout.write(buf)
             except Exception:
                 pass
-        time.sleep(opts.delay)
+        if opts.delay > 0.0:
+            time.sleep(opts.delay)
+        counter += 1
     except KeyboardInterrupt:
         sys.exit(0)
