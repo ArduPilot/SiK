d177e658f18fd8779bcf98166798ca4a804602a0
Andrew Tridgell <tridge@samba.org>
2012-01-15 21:44:41 +1100

    use a fixed number of channels

--- Firmware/radio/main.c
+++ Firmware/radio/main.c
@@ -74,9 +74,6 @@ __pdata uint8_t			g_board_bl_version;	///< from the bootloader
 ///
 static volatile uint8_t delay_counter;
 
-// how many frequency channels we have in our allowed ISM band
-static uint8_t num_freq_channels;
-
 /// Configure the Si1000 for operation.
 ///
 static void hardware_init(void);
@@ -217,9 +214,6 @@ radio_init(void)
 		break;
 	}
 
-	// use 50 channels to fit with the US regulations
-	num_freq_channels = 50;
-
 	// set the frequency and channel spacing
 	radio_set_frequency(freq);
 
@@ -227,7 +221,7 @@ radio_init(void)
 	radio_set_channel_spacing(250000UL);
 
 	// start on a channel chosen by network ID
-	radio_set_channel(param_get(PARAM_NETID) % num_freq_channels);
+	radio_set_channel(param_get(PARAM_NETID) % NUM_FREQ_CHANNELS);
 	
 	// And intilise the radio with them.
 	if (!radio_configure(param_get(PARAM_AIR_SPEED)*1000UL)) {
@@ -241,7 +235,7 @@ radio_init(void)
 	tdm_init();
 
 	// initialise frequency hopping system
-	fhop_init(num_freq_channels);
+	fhop_init(param_get(PARAM_NETID));
 }
 
 static void
