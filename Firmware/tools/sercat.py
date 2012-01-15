d3c12066e71cb89a875450727eb72d1fc475f115
Andrew Tridgell <tridge@samba.org>
2012-01-15 14:02:58 +1100

    some new test progs

---
+++ Firmware/tools/sercat.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# a trivial serial console
+
+import serial, sys, optparse
+
+parser = optparse.OptionParser("sercat")
+parser.add_option("--baudrate", type='int', default=115200, help='baud rate')
+
+opts, args = parser.parse_args()
+
+if len(args) != 1:
+    print("usage: console.py <DEVICE>")
+    sys.exit(1)
+
+device = args[0]
+
+port = serial.Serial(device, opts.baudrate, timeout=1,
+                     dsrdtr=False, rtscts=False, xonxoff=False)
+while True:
+    count = port.inWaiting()
+    if count == 0:
+        count = 1
+    buf = port.read(count)
+    sys.stdout.write(buf)
+    sys.stdout.flush()
+    
+
+
