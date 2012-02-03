b03b74d1be3dd1c7363522e4255e550671cf57f8
Andrew Tridgell <tridge@samba.org>
2012-02-03 15:50:29 +1100

    more tools

---
+++ Firmware/tools/set_sreg.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# set air data rate
+
+import serial, sys, optparse, time, fdpexpect
+
+parser = optparse.OptionParser("set_speed")
+parser.add_option("--baudrate", type='int', default=57600, help='baud rate')
+parser.add_option("--cmd", action='append', default=[], help='at command')
+parser.add_option("--reset", action='store_true', help='reset after set')
+parser.add_option("--write", action='store_true', help='write after set')
+
+opts, args = parser.parse_args()
+
+if len(args) == 0:
+    print("usage: set_sreg.py <DEVICE...>")
+    sys.exit(1)
+
+
+def set_speed(device):
+    '''set some registers'''
+    port = serial.Serial(device, opts.baudrate, timeout=0,
+                         dsrdtr=False, rtscts=False, xonxoff=False)
+    ser = fdpexpect.fdspawn(port.fileno(), logfile=sys.stdout)
+    ser.send('+++')
+    time.sleep(1)
+    ser.send('\r\nATI\r\n')
+    try:
+        ser.expect(['OK','SiK .* on HM-TRP'], timeout=2)
+    except fdpexpect.TIMEOUT:
+        print("timeout")
+        return
+    for cmd in opts.cmd:
+        ser.send('%s\r\n' % cmd)
+    ser.expect('OK')
+    if opts.write:
+        ser.send('AT&W\r\n')
+        ser.expect('OK')
+    if opts.reset:
+        ser.send('ATZ\r\n')
+    else:
+        ser.send('ATO\r\n')
+        ser.expect('OK')
+    time.sleep(1)
+    ser.read_nonblocking(300)
+    port.close()
+
+for d in args:
+    print("Setting %s on %s" % (opts.cmd, d))
+    set_speed(d)
+    
---
+++ Firmware/tools/show_rssi.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# set air data rate
+
+import serial, sys, optparse, time, fdpexpect
+
+parser = optparse.OptionParser("show_rssi")
+parser.add_option("--baudrate", type='int', default=57600, help='baud rate')
+
+opts, args = parser.parse_args()
+
+if len(args) == 0:
+    print("usage: show_rssi.py <DEVICE...>")
+    sys.exit(1)
+
+
+def show_rssi(device):
+    '''show RSSI'''
+    port = serial.Serial(device, opts.baudrate, timeout=0,
+                         dsrdtr=False, rtscts=False, xonxoff=False)
+    ser = fdpexpect.fdspawn(port.fileno(), logfile=sys.stdout)
+    ser.send('+++')
+    time.sleep(1)
+    ser.send('\r\nATI\r\n')
+    try:
+        ser.expect(['OK','SiK .* on HM-TRP'], timeout=2)
+    except fdpexpect.TIMEOUT:
+        print("timeout")
+        return
+    ser.send('AT&T=RSSI\r\n')
+    time.sleep(1)
+    ser.read_nonblocking(300)
+    ser.send('AT&T\r\n')
+    time.sleep(0.5)
+    ser.send('ATO\r\n')
+    ser.read_nonblocking(300)
+    print("")
+    time.sleep(1)
+    port.close()
+
+for d in args:
+    print("showing registers on %s" % (d))
+    show_rssi(d)
+    
