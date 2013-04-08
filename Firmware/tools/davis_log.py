f2cd4e319e95eb4c10ba7de95b85bff57e23dbe8
Andrew Tridgell <tridge@samba.org>
2013-04-08 19:02:18 +1000

    simple davis raw data logger

---
+++ Firmware/tools/davis_log.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# reflect input bytes to output, printing as it goes
+
+import serial, sys, optparse, time
+
+parser = optparse.OptionParser("reflector")
+parser.add_option("--baudrate", type='int', default=57600, help='baud rate')
+
+opts, args = parser.parse_args()
+
+if len(args) != 2:
+    print("usage: reflector.py <DEVICE> <logfile>")
+    sys.exit(1)
+
+device = args[0]
+logfile = args[1]
+
+port = serial.Serial(device, opts.baudrate, timeout=5, dsrdtr=False, rtscts=False, xonxoff=False)
+
+log = open(logfile, mode="w")
+
+while True:
+    line = port.readline()
+    line = line.rstrip()
+    out = "%s %.2f\n" % (line, time.time())
+    log.write(out);
+    log.flush()
+    sys.stdout.write(out)
+    sys.stdout.flush()
